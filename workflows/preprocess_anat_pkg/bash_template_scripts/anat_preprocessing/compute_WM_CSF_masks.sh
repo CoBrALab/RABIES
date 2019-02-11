93d95de34bd1c3832f272adeaf7c04c558d9a07f
Gab-D-G <gabriel.desrosiers-gregoire@mail.mcgill.ca>
2019-02-10 19:46:43 -0500

    cleaning anat preproc previous scripts

--- workflows/preprocess_anat_pkg/bash_template_scripts/anat_preprocessing/compute_WM_CSF_masks.sh
+++
@@ -1,7 +0,0 @@
-source activate custom_nipype
-
-mkdir -p WM_CSF_masks
-for file in mask_labels_files/*labels_resample.mnc; do var=$(basename $file); python /data/chamal/projects/Gabriel_DG/utils_scripts/compute_masks.py \
-$file WM_CSF_masks/${var:0:8} ; done
-
-for file in WM_CSF_masks/*_mask.nii.gz; do /data/chamal/projects/Gabriel_DG/utils_scripts/erode_mask.sh $file WM_CSF_masks/$(basename $file .nii.gz)_eroded.mnc; done
