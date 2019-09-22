db3d4f23a5a6fe791e8cc7f805d666160fde424a
Gab-D-G <34081874+Gab-D-G@users.noreply.github.com>
2019-09-22 15:27:32 -0400

    Local commonspace (#13)
    * Added ants_dbm cluster options.
    
    * Added pathos and tqdm dependencies.
    
    * Pydpiper specification.

--- rabies/shell_scripts/ants_dbm.sh
+++ rabies/shell_scripts/ants_dbm.sh
@@ -10,8 +10,8 @@ cd ants_dbm
 
 twolevel_dbm.py --rigid-model-target $atlas_labels \
 --no-N4 --transform SyN --float --average-type normmean --gradient-step 0.25 --model-iterations 3 \
---modelbuild-command antsMultivariateTemplateConstruction2.sh --cluster-type sge \
---walltime 40:00:00 --memory-request 30gb --local-threads 30 \
+--modelbuild-command antsMultivariateTemplateConstruction2.sh --cluster-type $ants_dbm_cluster_type \
+--walltime $ants_dbm_walltime --memory-request $ants_dbm_memory_request --local-threads $ants_dbm_local_threads \
 1level $FILE_PATH
 
 cd ..
