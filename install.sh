abca19511bf735e73467aa0ea570ec3d3b3bc8f6
Gabriel D-G <gabriel.desrosiers-gregoire@mail.mcgill.ca>
2020-01-22 17:17:13 -0500

    Fix for install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ export RABIES_VERSION=0.1.2-dev
 export RABIES=$HOME/RABIES-${RABIES_VERSION}
 export PYTHONPATH="${PYTHONPATH}:$RABIES"
 
-#cd $HOME
+cd $HOME
 #curl -L --retry 5 -o $HOME/RABIES.tar.gz https://github.com/CoBrALab/RABIES/archive/${RABIES_VERSION}.tar.gz
 #tar zxf $HOME/RABIES.tar.gz
 #rm $HOME/RABIES.tar.gz
