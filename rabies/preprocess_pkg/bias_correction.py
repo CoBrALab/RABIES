5a255fbd1c8a211f0603a264498487ad5c660549
Gab-D-G <gabriel.desrosiers-gregoire@mail.mcgill.ca>
2021-04-01 16:12:50 -0400

    Implemented mouse-preprocessing-v5.sh option for bold preprocessing too.

--- rabies/preprocess_pkg/bias_correction.py
+++ rabies/preprocess_pkg/bias_correction.py
@@ -15,7 +15,7 @@ def bias_correction_wf(opts, name='bias_correction_wf'):
 
     outputnode = pe.Node(
         niu.IdentityInterface(
-            fields=['corrected_EPI', 'denoise_mask', 'warped_EPI', 'init_denoise']),
+            fields=['corrected_EPI', 'denoise_mask', 'init_denoise']),
         name='outputnode')
 
     if opts.bold_bias_cor_method=='otsu_reg':
@@ -25,6 +25,31 @@ def bias_correction_wf(opts, name='bias_correction_wf'):
     elif opts.bold_bias_cor_method=='thresh_reg':
         bias_correction = pe.Node(ThreshBiasCorrection(rabies_data_type=opts.data_type),
                                   name='bias_correction', mem_gb=0.3*opts.scale_min_memory)
+    elif opts.bold_bias_cor_method=='mouse-preprocessing-v5.sh':
+        from nipype.interfaces.utility import Function
+        
+        def mouse_preprocess_func(input_ref_EPI,anat,anat_mask,name_source):
+            import os
+
+            import pathlib  # Better path manipulation
+            filename_split = pathlib.Path(
+                name_source).name.rsplit(".nii")
+            cwd = os.getcwd()
+            corrected_EPI = '%s/%s_bias_cor.nii.gz' % (cwd, filename_split[0],)
+
+            import rabies
+            dir_path = os.path.dirname(os.path.realpath(rabies.__file__))
+            from rabies.preprocess_pkg.utils import run_command
+            command = dir_path+'/shell_scripts/mouse-preprocessing-v5.sh %s %s %s %s' % (input_ref_EPI, corrected_EPI,anat,anat_mask)
+            rc = run_command(command)
+            denoise_mask = corrected_EPI.split('.nii.gz')[0]+'_mask.nii.gz'
+            init_denoise = corrected_EPI.split('.nii.gz')[0]+'_init_denoise.nii.gz'
+            return corrected_EPI, denoise_mask, init_denoise
+
+        bias_correction = pe.Node(Function(input_names=['input_ref_EPI','anat','anat_mask','name_source'],
+                                                    output_names=['corrected_EPI', 'denoise_mask', 'init_denoise'],
+                                                    function=mouse_preprocess_func),
+                                           name='bold_convert_to_RAS')
     else:
         raise ValueError("Wrong --bold_bias_cor_method.")
 
@@ -35,7 +60,6 @@ def bias_correction_wf(opts, name='bias_correction_wf'):
                                       ('name_source', 'name_source'),
                                       ]),
         (bias_correction, outputnode, [('corrected_EPI', 'corrected_EPI'),
-                                       ('warped_EPI', 'warped_EPI'),
                                        ('denoise_mask', 'denoise_mask'),
                                        ('init_denoise', 'init_denoise'),
                                        ]),
@@ -95,13 +119,8 @@ class OtsuEPIBiasCorrection(BaseInterface):
             cwd, filename_split[0])
         biascor_EPI = '%s/%s_bias_cor.nii.gz' % (cwd, filename_split[0],)
 
-        # resample to isotropic resolution based on lowest dimension
         input_ref_EPI_img = sitk.ReadImage(
             self.inputs.input_ref_EPI, self.inputs.rabies_data_type)
-        dim = input_ref_EPI_img.GetSpacing()
-        low_dim = np.asarray(dim).min()
-        #sitk.WriteImage(resample_image_spacing(
-        #    input_ref_EPI_img, (low_dim, low_dim, low_dim)), resampled)
 
         # the -b will be rounded up to the nearest multiple of 10 of the image largest dimension
         largest_dim = (np.array(input_ref_EPI_img.GetSize())*np.array(input_ref_EPI_img.GetSpacing())).max()
