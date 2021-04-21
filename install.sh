409aebcfe8ab4d828fe1fd23a54e68deda9d28d5
Gab-D-G <gabriel.desrosiers-gregoire@mail.mcgill.ca>
2021-04-21 13:37:02 -0400

    setup.py is now standalone. Template files get downloaded into the rabies installation folder when running the first time.

--- install.sh
+++ install.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-# install twolevel_ants_dbm
-git clone https://github.com/CoBrALab/twolevel_ants_dbm.git $PWD/twolevel_ants_dbm
-cd $PWD/twolevel_ants_dbm
-git checkout 3623b5bf683473887b7d63c1a1b24c0362ed8396
-
-# install twolevel_ants_dbm
-git clone https://github.com/CoBrALab/minc-toolkit-extras.git $PWD/minc-toolkit-extras
-cd $PWD/minc-toolkit-extras
-git checkout 3dec9b81b0e59c7daa90ea1901469111b2374182
+git clone --origin 3623b5bf683473887b7d63c1a1b24c0362ed8396 https://github.com/CoBrALab/twolevel_ants_dbm.git
+git clone --origin 3dec9b81b0e59c7daa90ea1901469111b2374182 https://github.com/CoBrALab/minc-toolkit-extras.git
--- scripts/install_DSURQE.sh
+++ scripts/install_DSURQE.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
+out_dir=$1
+tmpdir=$(mktemp -d)
+mkdir -p $out_dir
+
 # Download DSURQE template
-template_dir=$RABIES/template_files
-curl -L --retry 5 -o $template_dir/DSURQE_40micron_average.nii http://repo.mouseimaging.ca/repo/DSURQE_40micron_nifti/DSURQE_40micron_average.nii
-curl -L --retry 5 -o $template_dir/DSURQE_40micron_labels.nii http://repo.mouseimaging.ca/repo/DSURQE_40micron_nifti/DSURQE_40micron_labels.nii
-#curl -L --retry 5 -o $template_dir/DSURQE_40micron_mask.nii http://repo.mouseimaging.ca/repo/DSURQE_40micron_nifti/DSURQE_40micron_mask.nii
-curl -L --retry 5 -o $template_dir/DSURQE_40micron_R_mapping.csv http://repo.mouseimaging.ca/repo/DSURQE_40micron/DSURQE_40micron_R_mapping.csv
+curl -L --retry 5 -o ${tmpdir}/DSURQE_40micron_average.mnc http://repo.mouseimaging.ca/repo/DSURQE_40micron/DSURQE_40micron_average.mnc
+curl -L --retry 5 -o ${tmpdir}/DSURQE_40micron_labels.mnc http://repo.mouseimaging.ca/repo/DSURQE_40micron/DSURQE_40micron_labels.mnc
+curl -L --retry 5 -o ${tmpdir}/DSURQE_40micron_mask.mnc http://repo.mouseimaging.ca/repo/DSURQE_40micron/DSURQE_40micron_mask.mnc
+curl -L --retry 5 -o ${out_dir}/DSURQE_40micron_R_mapping.csv http://repo.mouseimaging.ca/repo/DSURQE_40micron/DSURQE_40micron_R_mapping.csv
 
-# create a 100um version
-ResampleImage 3 $template_dir/DSURQE_40micron_average.nii $template_dir/DSURQE_100micron_average.nii 0.1x0.1x0.1 0 4
-#antsApplyTransforms -i $template_dir/DSURQE_40micron_mask.nii -o $template_dir/DSURQE_100micron_mask.nii -r $template_dir/DSURQE_100micron_average.nii -n GenericLabel
-antsApplyTransforms -i $template_dir/DSURQE_40micron_labels.nii -o $template_dir/DSURQE_100micron_labels.nii -r $template_dir/DSURQE_100micron_average.nii -n GenericLabel
+# convert to nifti
+mnc2nii ${tmpdir}/DSURQE_40micron_average.mnc ${out_dir}/DSURQE_40micron_average.nii
+mnc2nii ${tmpdir}/DSURQE_40micron_labels.mnc ${out_dir}/DSURQE_40micron_labels.nii
+mnc2nii ${tmpdir}/DSURQE_40micron_mask.mnc ${out_dir}/DSURQE_40micron_mask.nii
 
-gzip -f $template_dir/*.nii
-
-#convert templates to the RAS axis convention
-python $RABIES/convert_to_RAS.py $template_dir/DSURQE_100micron_average.nii.gz
-python $RABIES/convert_to_RAS.py $template_dir/DSURQE_100micron_mask.nii.gz
-python $RABIES/convert_to_RAS.py $template_dir/DSURQE_100micron_labels.nii.gz
+gzip -f ${out_dir}/DSURQE_40micron_*.nii
 
 # create regional masks
-gen_DSURQE_masks.py $template_dir/DSURQE_100micron_labels.nii.gz $template_dir/DSURQE_40micron_R_mapping.csv $template_dir/DSURQE_100micron
+gen_DSURQE_masks.py ${out_dir}/DSURQE_40micron_labels.nii.gz ${out_dir}/DSURQE_40micron_R_mapping.csv ${out_dir}/DSURQE_40micron
+
+# download additional supportive files
+curl -L --retry 5 https://zenodo.org/record/4706153/files/melodic_IC.nii.gz -o ${out_dir}/melodic_IC.nii.gz
+curl -L --retry 5 https://zenodo.org/record/4706153/files/vascular_mask.nii.gz -o ${out_dir}/vascular_mask.nii.gz
+
+rm -rf ${tmpdir}
