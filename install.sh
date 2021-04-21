246bd8fc0a0898211669b97251660971ae29e991
Gab-D-G <gabriel.desrosiers-gregoire@mail.mcgill.ca>
2021-04-21 16:11:26 -0400

    Updated to new version for pypi update. Removed install.sh, INSTALL.md and rabies_environment.yml

--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-git clone --origin 3623b5bf683473887b7d63c1a1b24c0362ed8396 https://github.com/CoBrALab/twolevel_ants_dbm.git
-git clone --origin 3dec9b81b0e59c7daa90ea1901469111b2374182 https://github.com/CoBrALab/minc-toolkit-extras.git
