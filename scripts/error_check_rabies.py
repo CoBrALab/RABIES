b84f5339275ab7609825685f70be2195cbeb240c
Gab-D-G <gabriel.desrosiers-gregoire@mail.mcgill.ca>
2021-06-01 16:56:52 -0400

    Debugged file generation for the error check script

--- scripts/error_check_rabies.py
+++ scripts/error_check_rabies.py
@@ -24,11 +24,15 @@ spacing = (float(1), float(1), float(1)) # resample to 1mmx1mmx1mm
 resampled = resample_image_spacing(sitk.ReadImage(template), spacing)
 array = sitk.GetArrayFromImage(resampled)
 array_4d = np.repeat(array[:,:,:,np.newaxis], 15, axis=3)
-#array_4d += np.random.normal(0,array_4d.mean(),array_4d.shape) # add gaussian noise
+array_4d += np.random.normal(0,array_4d.mean()/1000,array_4d.shape) # add gaussian noise
 sitk.WriteImage(resampled, tmppath+'/inputs/sub-token_T1w.nii.gz')
 sitk.WriteImage(sitk.GetImageFromArray(array_4d, isVector=False), tmppath+'/inputs/sub-token_bold.nii.gz')
+
 resampled = resample_image_spacing(sitk.ReadImage(mask), spacing)
-sitk.WriteImage(resampled, tmppath+'/inputs/token_mask.nii.gz')
+array = sitk.GetArrayFromImage(resampled).astype(bool).astype(int)
+binarized = sitk.GetImageFromArray(array, isVector=False)
+binarized.CopyInformation(resampled)
+sitk.WriteImage(binarized, tmppath+'/inputs/token_mask.nii.gz')
 
 sitk.WriteImage(copyInfo_4DImage(sitk.ReadImage(tmppath+'/inputs/sub-token_bold.nii.gz'), sitk.ReadImage(tmppath+'/inputs/sub-token_T1w.nii.gz'), sitk.ReadImage(tmppath+'/inputs/sub-token_bold.nii.gz')), tmppath+'/inputs/sub-token_bold.nii.gz')
 
