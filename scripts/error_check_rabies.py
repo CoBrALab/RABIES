444a0ba643b9fc6d7d67ff792420054585d8010b
Gabriel D-G <gabriel.desrosiers-gregoire@mail.mcgill.ca>
2021-06-01 12:46:17 -0400

    Corrected bug with the --dummy_scan option.

--- scripts/error_check_rabies.py
+++ scripts/error_check_rabies.py
@@ -20,18 +20,19 @@ template="%s/DSURQE_40micron_average.nii.gz" % (rabies_path)
 mask="%s/DSURQE_40micron_mask.nii.gz" % (rabies_path)
 
 img = sitk.ReadImage(template)
-spacing = (float(1), float(1), float(1)) # resample to 1mmx1mmx1mm
+spacing = (float(2), float(2), float(2)) # resample to 1mmx1mmx1mm
 resampled = resample_image_spacing(sitk.ReadImage(template), spacing)
 array = sitk.GetArrayFromImage(resampled)
 array_4d = np.repeat(array[:,:,:,np.newaxis], 15, axis=3)
+#array_4d += np.random.normal(0,array_4d.mean(),array_4d.shape) # add gaussian noise
 sitk.WriteImage(resampled, tmppath+'/inputs/sub-token_T1w.nii.gz')
 sitk.WriteImage(sitk.GetImageFromArray(array_4d, isVector=False), tmppath+'/inputs/sub-token_bold.nii.gz')
 resampled = resample_image_spacing(sitk.ReadImage(mask), spacing)
 sitk.WriteImage(resampled, tmppath+'/inputs/token_mask.nii.gz')
 
 command = "rabies preprocess %s/inputs %s/outputs --debug --anat_bias_cor_method disable --bold_bias_cor_method disable \
-    --coreg_script null_nonlin --template_reg_script null_nonlin --data_type int16 -e --detect_dummy \
-    --HMC_transform Affine --tpattern seq" % (tmppath,tmppath)
+    --anat_template %s/inputs/sub-token_T1w.nii.gz --brain_mask %s/inputs/token_mask.nii.gz --WM_mask %s/inputs/token_mask.nii.gz --CSF_mask %s/inputs/token_mask.nii.gz --vascular_mask %s/inputs/token_mask.nii.gz --labels %s/inputs/token_mask.nii.gz \
+    --coreg_script null_nonlin --template_reg_script null_nonlin --data_type int16" % (tmppath,tmppath,tmppath,tmppath,tmppath,tmppath,tmppath,tmppath)
 process = subprocess.run(
     command,
     check=True,
@@ -40,13 +41,14 @@ process = subprocess.run(
 
 
 command = "rabies preprocess %s/inputs %s/outputs --debug --anat_bias_cor_method disable --bold_bias_cor_method disable \
-    --coreg_script null_nonlin --template_reg_script null_nonlin --data_type int16" % (tmppath,tmppath)
+    --anat_template %s/inputs/sub-token_T1w.nii.gz --brain_mask %s/inputs/token_mask.nii.gz --WM_mask %s/inputs/token_mask.nii.gz --CSF_mask %s/inputs/token_mask.nii.gz --vascular_mask %s/inputs/token_mask.nii.gz --labels %s/inputs/token_mask.nii.gz \
+    --coreg_script null_nonlin --template_reg_script null_nonlin --data_type int16 -e --detect_dummy \
+    --HMC_transform Affine --tpattern seq" % (tmppath,tmppath,tmppath,tmppath,tmppath,tmppath,tmppath,tmppath)
 process = subprocess.run(
     command,
     check=True,
     shell=True,
     )
-
 
 
 shutil.rmtree(tmppath)
