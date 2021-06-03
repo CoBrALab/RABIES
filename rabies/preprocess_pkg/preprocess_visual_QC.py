f32f54f8c3c13858964dcfd1655233f020d59d88
Gab-D-G <gabriel.desrosiers-gregoire@mail.mcgill.ca>
2021-06-03 14:51:33 -0400

    Added white titles

--- rabies/preprocess_pkg/preprocess_visual_QC.py
+++ rabies/preprocess_pkg/preprocess_visual_QC.py
@@ -63,15 +63,15 @@ class PlotOverlap(BaseInterface):
         return {'out_png': getattr(self, 'out_png')}
 
 
-def otsu_scaling(image):
+def otsu_scaling(image_file):
     import numpy as np
     import SimpleITK as sitk
-    img = sitk.ReadImage(image)
+    img = sitk.ReadImage(image_file)
     array = sitk.GetArrayFromImage(img)
 
     # select a smart vmax for the image display to enhance contrast
     from rabies.preprocess_pkg.utils import run_command
-    command = 'ThresholdImage 3 %s otsu_weight.nii.gz Otsu 4' % (image)
+    command = 'ThresholdImage 3 %s otsu_weight.nii.gz Otsu 4' % (image_file)
     rc = run_command(command)
 
     # clip off the background
@@ -186,7 +186,7 @@ def template_info(anat_template, opts, out_dir):
 
     fig,axes = plt.subplots(nrows=3, ncols=6, figsize=(4*6,2*2))
 
-    axes[0,0].set_title('Anatomical Template', fontsize=20)
+    axes[0,0].set_title('Anatomical Template', fontsize=30, color='white')
     plot_3d(axes[:,0],scaled,fig=fig,vmin=0,vmax=1,cmap='gray')
     # plot brain mask
     mask = brain_mask
@@ -194,7 +194,7 @@ def template_info(anat_template, opts, out_dir):
         mask, sitk.sitkFloat32)
     # resample mask to match template
     sitk_mask = sitk.Resample(sitk_mask, scaled)
-    axes[0,1].set_title('Brain Mask', fontsize=20)
+    axes[0,1].set_title('Brain Mask', fontsize=30, color='white')
     plot_3d(axes[:,1],scaled,fig=fig,vmin=0,vmax=1,cmap='gray')
     plot_3d(axes[:,1],sitk_mask,fig=fig,vmin=-1,vmax=1,cmap='bwr', alpha=0.3, cbar=False)
     # plot WM mask
@@ -203,7 +203,7 @@ def template_info(anat_template, opts, out_dir):
         mask, sitk.sitkFloat32)
     # resample mask to match template
     sitk_mask = sitk.Resample(sitk_mask, scaled)
-    axes[0,2].set_title('WM Mask', fontsize=20)
+    axes[0,2].set_title('WM Mask', fontsize=30, color='white')
     plot_3d(axes[:,2],scaled,fig=fig,vmin=0,vmax=1,cmap='gray')
     plot_3d(axes[:,2],sitk_mask,fig=fig,vmin=-1,vmax=1,cmap='bwr', alpha=0.5, cbar=False)
     # plot CSF mask
@@ -212,7 +212,7 @@ def template_info(anat_template, opts, out_dir):
         mask, sitk.sitkFloat32)
     # resample mask to match template
     sitk_mask = sitk.Resample(sitk_mask, scaled)
-    axes[0,3].set_title('CSF Mask', fontsize=20)
+    axes[0,3].set_title('CSF Mask', fontsize=30, color='white')
     plot_3d(axes[:,3],scaled,fig=fig,vmin=0,vmax=1,cmap='gray')
     plot_3d(axes[:,3],sitk_mask,fig=fig,vmin=-1,vmax=1,cmap='bwr', alpha=0.5, cbar=False)
     # plot VASC mask
@@ -221,7 +221,7 @@ def template_info(anat_template, opts, out_dir):
         mask, sitk.sitkFloat32)
     # resample mask to match template
     sitk_mask = sitk.Resample(sitk_mask, scaled)
-    axes[0,4].set_title('Vascular Mask', fontsize=20)
+    axes[0,4].set_title('Vascular Mask', fontsize=30, color='white')
     plot_3d(axes[:,4],scaled,fig=fig,vmin=0,vmax=1,cmap='gray')
     plot_3d(axes[:,4],sitk_mask,fig=fig,vmin=-1,vmax=1,cmap='bwr', alpha=0.5, cbar=False)
 
@@ -231,7 +231,7 @@ def template_info(anat_template, opts, out_dir):
         mask, sitk.sitkFloat32)
     # resample mask to match template
     sitk_mask = sitk.Resample(sitk_mask, scaled)
-    axes[0,5].set_title('Atlas Labels', fontsize=20)
+    axes[0,5].set_title('Atlas Labels', fontsize=30, color='white')
     plot_3d(axes[:,5],scaled,fig=fig,vmin=0,vmax=1,cmap='gray')
     plot_3d(axes[:,5],sitk_mask,fig=fig,vmin=1,vmax=sitk.GetArrayFromImage(sitk_mask).max(),cmap='rainbow', alpha=0.5, cbar=False)
     plt.tight_layout()
@@ -261,18 +261,18 @@ def temporal_features(bold_file, confounds_csv, FD_csv, rabies_data_type, name_s
     ax.plot(df['mov2'])
     ax.plot(df['mov3'])
     ax.legend(['mov1','mov2','mov3'])
-    ax.set_title('Translation parameters', fontsize=20)
+    ax.set_title('Translation parameters', fontsize=30, color='white')
     ax = axes[1,0]
     ax.plot(df['rot1'])
     ax.plot(df['rot2'])
     ax.plot(df['rot3'])
     ax.legend(['rot1','rot2','rot3'])
-    ax.set_title('Rotation parameters', fontsize=20)
+    ax.set_title('Rotation parameters', fontsize=30, color='white')
 
     df = pd.read_csv(FD_csv)
     ax=axes[2,0]
     ax.plot(df['Mean'], color='r')
-    ax.set_title('Framewise Displacement', fontsize=20)
+    ax.set_title('Framewise Displacement', fontsize=30, color='white')
 
     plt.tight_layout()
 
@@ -293,12 +293,12 @@ def temporal_features(bold_file, confounds_csv, FD_csv, rabies_data_type, name_s
         sitk.GetImageFromArray(tSNR, isVector=False), img)
     sitk.WriteImage(tSNR_image, tSNR_filename)
 
-    axes[0,1].set_title('Temporal STD', fontsize=20)
+    axes[0,1].set_title('Temporal STD', fontsize=30, color='white')
     std=std.flatten()
     std.sort()
     std_vmax = std[int(len(std)*0.95)]
     plot_3d(axes[:,1],std_image,fig=fig,vmin=0,vmax=std_vmax,cmap='inferno', cbar=True)
-    axes[0,2].set_title('Temporal SNR', fontsize=20)
+    axes[0,2].set_title('Temporal SNR', fontsize=30, color='white')
     plot_3d(axes[:,2],tSNR_image,fig=fig,vmin=0,vmax=tSNR.max(),cmap='Spectral', cbar=True)
 
     fig.savefig('%s_temporal_features.png' % (prefix), bbox_inches='tight')
@@ -316,27 +316,46 @@ def denoising_diagnosis(raw_img,init_denoise,warped_mask,final_denoise, name_sou
         filename_template
 
     import matplotlib.pyplot as plt
-    from rabies.preprocess_pkg.preprocess_visual_QC import plot_3d,otsu_scaling
+    from rabies.preprocess_pkg.preprocess_visual_QC import plot_3d,otsu_scaling, add_filenames
     fig,axes = plt.subplots(nrows=3, ncols=4, figsize=(12*4,2*3))
-    plt.tight_layout()
 
     scaled = otsu_scaling(raw_img)
-    axes[0,0].set_title('Raw EPI', fontsize=20)
+    axes[0,0].set_title('Raw EPI', fontsize=30, color='white')
+    #add_filenames(axes[-1,0], {'File':raw_img})
     plot_3d(axes[:,0],scaled,fig=fig,vmin=0,vmax=1,cmap='viridis')
 
-    scaled = otsu_scaling(init_denoise)
-    axes[0,1].set_title('Initial Denoising', fontsize=20)
-    plot_3d(axes[:,1],scaled,fig=fig,vmin=0,vmax=1,cmap='viridis')
-
-    axes[0,2].set_title('Resampled Mask', fontsize=20)
+    axes[0,2].set_title('Resampled Mask', fontsize=30, color='white')
+    #add_filenames(axes[-1,2], {'Mask File':warped_mask,'EPI File':raw_img})
     plot_3d(axes[:,2],scaled,fig=fig,vmin=0,vmax=1,cmap='viridis')
     sitk_mask = sitk.ReadImage(warped_mask,sitk.sitkFloat32)
     # resample mask to match template
     sitk_mask = sitk.Resample(sitk_mask, scaled)
     plot_3d(axes[:,2],sitk_mask,fig=fig,vmin=-1,vmax=1,cmap='bwr', alpha=0.3, cbar=False)
 
+    scaled = otsu_scaling(init_denoise)
+    axes[0,1].set_title('Initial Denoising', fontsize=30, color='white')
+    #add_filenames(axes[-1,1], {'File':init_denoise})
+    plot_3d(axes[:,1],scaled,fig=fig,vmin=0,vmax=1,cmap='viridis')
+
     scaled = otsu_scaling(final_denoise)
-    axes[0,3].set_title('Final Denoising', fontsize=20)
+    axes[0,3].set_title('Final Denoising', fontsize=30, color='white')
+    #add_filenames(axes[-1,3], {'File':final_denoise})
     plot_3d(axes[:,3],scaled,fig=fig,vmin=0,vmax=1,cmap='viridis')
 
+    plt.tight_layout()
     fig.savefig('%s_denoising.png' % (prefix), bbox_inches='tight')
+
+def add_filenames(ax, file_dict, line_length=40):
+    txt=""
+    for key in list(file_dict.keys()):
+        txt+=key+": "
+        file=file_dict[key]
+        i=0
+        while(i<len(file)):
+            txt+=file[i:i+line_length]+"\n"
+            i+=line_length
+
+    ax.text(0.5, -0.5,txt[:10], color='white', fontsize=15,
+         horizontalalignment='center',
+         verticalalignment='bottom',
+         transform = ax.transAxes)
