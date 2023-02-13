Better handling of scaling. Fixed bad CRsd scaling in temporal report.

rabies/analysis_pkg/diagnosis_pkg/diagnosis_functions.py
@@ -367,12 +367,11 @@ def scan_diagnosis(data_dict, temporal_info, spatial_info, regional_grayplot=Fal
         if len(network_time)>0:
             # make sure the timecourses are normalized
             network_time = network_time.copy()
-            #network_time /= np.sqrt((network_time ** 2).sum(axis=0))
-            network_time /= network_time.std(axis=0)
+            network_time /= np.sqrt((network_time ** 2).mean(axis=0))
             network_time_avg = np.abs(network_time).mean(axis=1)
             # we introduce a scaler to prevent overlap of timecourses
             network_time_avg += scaler
-            scaler -= 2
+            scaler -= 1
             ax4.plot(x,network_time_avg, color=color, alpha=0.8)
             legend.append(name)
 

rabies/confound_correction_pkg/confound_correction.py
@@ -420,44 +420,24 @@ class Regress(BaseInterface):
 
         if cr_opts.image_scaling=='global_variance':
             scaling_factor = timeseries.std()
-            timeseries = timeseries/scaling_factor
-            # we scale also the variance estimates from CR
-            predicted = predicted/scaling_factor
-            predicted_random = predicted_random/scaling_factor
         elif cr_opts.image_scaling=='grand_mean_scaling':
-            scaling_factor = grand_mean
-            timeseries = timeseries/scaling_factor
-            timeseries *= 100 # we scale BOLD in % fluctuations
-            # we scale also the variance estimates from CR
-            predicted = predicted/scaling_factor
-            predicted_random = predicted_random/scaling_factor
+            scaling_factor = grand_mean/100 # we scale BOLD in % fluctuations, hence dividing by 100
         elif cr_opts.image_scaling=='voxelwise_standardization':
             # each voxel is scaled according to its STD
-            temporal_std = timeseries.std(axis=0) 
-            timeseries = timeseries/temporal_std
-            nan_voxels = np.isnan(timeseries).sum(axis=0)>1
-            timeseries[:,nan_voxels] = 0
-            # we scale also the variance estimates from CR
-            predicted = predicted/temporal_std
-            nan_voxels = np.isnan(predicted).sum(axis=0)>1
-            predicted[:,nan_voxels] = 0
-            predicted_random = predicted_random/temporal_std
-            nan_voxels = np.isnan(predicted_random).sum(axis=0)>1
-            predicted_random[:,nan_voxels] = 0
+            scaling_factor = timeseries.std(axis=0) 
         elif cr_opts.image_scaling=='voxelwise_mean':
-            # each voxel is scaled according to its mean
-            timeseries = timeseries/voxelwise_mean
-            nan_voxels = np.isnan(timeseries).sum(axis=0)>1
-            timeseries[:,nan_voxels] = 0
-            timeseries *= 100 # we scale BOLD in % fluctuations
+            scaling_factor = voxelwise_mean/100 # we scale BOLD in % fluctuations, hence dividing by 100
+        else:
+            scaling_factor = 1
 
-            # we scale also the variance estimates from CR
-            predicted = predicted/voxelwise_mean
-            nan_voxels = np.isnan(predicted).sum(axis=0)>1
-            predicted[:,nan_voxels] = 0
-            predicted_random = predicted_random/voxelwise_mean
-            nan_voxels = np.isnan(predicted_random).sum(axis=0)>1
-            predicted_random[:,nan_voxels] = 0
+        # scale fMRI timeseries, as well as the CR prediction to keep consistent scaling
+        scaled_list = []
+        for temp_timeseries in [timeseries,predicted,predicted_random]:
+            temp_timeseries /= scaling_factor
+            nan_voxels = np.isnan(temp_timeseries).sum(axis=0)>1
+            temp_timeseries[:,nan_voxels] = 0
+            scaled_list.append(temp_timeseries)
+        [timeseries,predicted,predicted_random] = scaled_list
 
         # after variance scaling, compute the variability estimates
         temporal_std = timeseries.std(axis=0)