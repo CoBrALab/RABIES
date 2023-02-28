788e22d4e0da41a5cc432a2f3a66c84362fae2ad
Gab-D-G <gabriel.desrosiers-gregoire@mail.mcgill.ca>
2023-02-28 15:31:50 -0500

    Added STC to error checks.

--- scripts/error_check_rabies.py
+++ scripts/error_check_rabies.py
@@ -56,7 +56,7 @@ sitk.WriteImage(copyInfo_4DImage(sitk.ReadImage(tmppath+'/inputs/sub-token_bold.
 command = f"rabies --verbose 1 preprocess {tmppath}/inputs {tmppath}/outputs --anat_inho_cor method=disable,otsu_thresh=2,multiotsu=false --bold_inho_cor method=disable,otsu_thresh=2,multiotsu=false \
     --anat_template {tmppath}/inputs/sub-token_T1w.nii.gz --brain_mask {tmppath}/inputs/token_mask.nii.gz --WM_mask {tmppath}/inputs/token_mask.nii.gz --CSF_mask {tmppath}/inputs/token_mask.nii.gz --vascular_mask {tmppath}/inputs/token_mask.nii.gz --labels {tmppath}/inputs/token_mask.nii.gz \
     --bold2anat_coreg registration=no_reg,masking=false,brain_extraction=false --commonspace_reg masking=false,brain_extraction=false,fast_commonspace=true,template_registration=no_reg --data_type int16 --bold_only --detect_dummy \
-    --tpattern seq-z"
+    --tpattern seq-z --apply_STC"
 process = subprocess.run(
     command,
     check=True,
