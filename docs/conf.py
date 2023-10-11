0e3508f66540dc56474981a7b223163d5adeb21a
Gab-D-G <gabriel.desrosiers-gregoire@mail.mcgill.ca>
2023-10-11 15:52:22 -0400

    Update documentation rabies version to 0.5.1

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, CoBrALab and Gabriel Desrosiers-Gregoire and Gabriel A. Deven
 author = 'CoBrALab'
 
 # The full version, including alpha/beta/rc tags
-release = '0.5.0'
+release = '0.5.1'
 
 
 # -- General configuration ---------------------------------------------------
