aae09ccbf63097d852d104121827a998a0039698
Gab-D-G <gabriel.desrosiers-gregoire@mail.mcgill.ca>
2025-12-18 19:30:39 +0100

    Integration of simpleitk motion correction scripts into the head motion correction and 3D gen ref. functions. Switched the submodule to the Gab-D-G temporary fork repository.

--- rabies/preprocess_pkg/hmc.py
+++ rabies/preprocess_pkg/hmc.py
@@ -1,12 +1,9 @@
 import os
 import pathlib
-import SimpleITK as sitk
-import numpy as np
 from nipype.interfaces.base import (
     traits, TraitedSpec, BaseInterfaceInputSpec,
     File, BaseInterface
 )
-from rabies.utils import run_command
 from nipype.pipeline import engine as pe
 from nipype.interfaces import utility as niu
 
@@ -58,7 +55,7 @@ def init_bold_hmc_wf(opts, name='bold_hmc_wf'):
         name='outputnode')
 
     # Head motion correction (hmc)
-    motion_estimation = pe.Node(antsMotionCorr(prebuilt_option=opts.HMC_option,transform_type='Rigid', second=False, rabies_data_type=opts.data_type),
+    motion_estimation = pe.Node(sitkMotionCorr(prebuilt_option=opts.HMC_option,transform_type='Rigid', second=False, rabies_data_type=opts.data_type),
                          name='ants_MC', mem_gb=1.1*opts.scale_min_memory, n_procs=int(os.environ['RABIES_ITK_NUM_THREADS']))
     motion_estimation.plugin_args = {
         'qsub_args': f'-pe smp {str(3*opts.min_proc)}', 'overwrite': True}
@@ -90,18 +87,16 @@ def init_bold_hmc_wf(opts, name='bold_hmc_wf'):
     return workflow
 
 
-class antsMotionCorrInputSpec(BaseInterfaceInputSpec):
+class sitkMotionCorrInputSpec(BaseInterfaceInputSpec):
     in_file = File(exists=True, mandatory=True, desc='input BOLD time series')
     ref_file = File(exists=True, mandatory=True,
                     desc='ref file to realignment time series')
-    prebuilt_option = traits.Str(desc="Select one of the pre-built options from https://github.com/ANTsX/ANTsR/blob/60eefd96fedd16bceb4703ecd2cd5730e6843807/R/ants_motion_estimation.R")
-    transform_type = traits.Str(desc="Specify between Rigid and Affine transform.")
-    second = traits.Bool(desc="specify if it is the second iteration")
+    level = traits.Int(desc="Select a level from 0 to 3, where each level is more stringent registration.")
     rabies_data_type = traits.Int(mandatory=True,
                                   desc="Integer specifying SimpleITK data type.")
 
 
-class antsMotionCorrOutputSpec(TraitedSpec):
+class sitkMotionCorrOutputSpec(TraitedSpec):
     mc_corrected_bold = File(exists=True, desc="motion corrected time series")
     avg_image = File(
         exists=True, desc="average image of the motion corrected time series")
@@ -109,114 +104,28 @@ class antsMotionCorrOutputSpec(TraitedSpec):
         exists=True, desc="csv files with the 6-parameters rigid body transformations")
 
 
-class antsMotionCorr(BaseInterface):
+class sitkMotionCorr(BaseInterface):
     """
-    This interface performs motion realignment using antsMotionCorr function. It takes a reference volume to which
+    This interface performs motion realignment using simpleitk_timeseries_motion_correction. It takes a reference volume to which
     EPI volumes from the input 4D file are realigned based on a Rigid registration.
     """
 
-    input_spec = antsMotionCorrInputSpec
-    output_spec = antsMotionCorrOutputSpec
+    input_spec = sitkMotionCorrInputSpec
+    output_spec = sitkMotionCorrOutputSpec
 
     def _run_interface(self, runtime):
- 
-        # change the name of the first iteration directory to prevent overlap of files with second iteration
-        if self.inputs.second:
-            command = 'mv ants_mc_tmp first_ants_mc_tmp'
-            rc,c_out = run_command(command)
-
-        # make a tmp directory to store the files
-        os.makedirs('ants_mc_tmp', exist_ok=True)
-
-        # adaptation from https://github.com/ANTsX/ANTsR/blob/60eefd96fedd16bceb4703ecd2cd5730e6843807/R/ants_motion_estimation.R
+        from rabies.simpleitk_timeseries_motion_correction.motion import framewise_register_pair, write_transforms_to_csv
         moving = self.inputs.in_file
-        fixed = self.inputs.ref_file
-        txtype = self.inputs.transform_type
-        moreaccurate = self.inputs.prebuilt_option
-        verbose = 0
+        ref_file = self.inputs.ref_file
+        level = self.inputs.level
+        filename_split = pathlib.Path(moving).name.rsplit(".nii")
+        output_prefix = os.path.abspath(
+            f'{filename_split[0]}_')
 
-        if txtype not in ['Rigid', 'Affine']:
-            raise ValueError("Wrong transform type provided.")
-        if not (moreaccurate=="intraSubjectBOLD" or moreaccurate=="optim"):
-            moreaccurate=int(moreaccurate)
-            if moreaccurate not in [0, 1, 2, 3]:
-                raise ValueError("Wrong pre-built option provided.")
-
-        img = sitk.ReadImage(moving, self.inputs.rabies_data_type)
-        ref_img = sitk.ReadImage(fixed, self.inputs.rabies_data_type)
-
-        n = img.GetSize()[3]
-        if (n > 10):
-            n = 10
-        mibins = 20
-
-        # check the size of the lowest dimension, and make sure that the first shrinking factor allow for at least 4 slices
-        shrinking_factor = 4
-        low_dim = np.asarray(ref_img.GetSize()[:3]).min()
-        if shrinking_factor > int(low_dim/4):
-            shrinking_factor = int(low_dim/4)
-
-        if (moreaccurate == 3):
-            command = f"antsMotionCorr -d 3 -o [ants_mc_tmp/motcorr,ants_mc_tmp/motcorr.nii.gz,ants_mc_tmp/motcorr_avg.nii.gz] -m MI[ {fixed} , \
-                {moving} , 1 , {str(mibins)} ] -t {txtype}[0.1,3,0] -i 100x50x30 -s 2x1x0mm -f {str(shrinking_factor)}x2x1 -u 1 -e 1 -n {str(n)} -v {str(verbose)}"
-
-        elif (moreaccurate == 2):
-            command = f"antsMotionCorr -d 3 -o [ants_mc_tmp/motcorr,ants_mc_tmp/motcorr.nii.gz,ants_mc_tmp/motcorr_avg.nii.gz] -m MI[ {fixed} , \
-                {moving} , 1 , {str(mibins)} , regular, 0.25 ] -t {txtype}[ 0.1 ] -i 100x50x30 -s 2x1x0mm -f {str(shrinking_factor)}x2x1 -u 1 -e 1 -n {str(n)} -v {str(verbose)}"
-
-        elif (moreaccurate == "intraSubjectBOLD"):
-            command = f"antsMotionCorr -d 3 -o [ants_mc_tmp/motcorr,ants_mc_tmp/motcorr.nii.gz,ants_mc_tmp/motcorr_avg.nii.gz] -m MI[ {fixed} , \
-                {moving} , 1 , {str(mibins)} , regular, 0.2 ] -t {txtype}[ 0.25 ] -i 50x20 -s 1x0mm -f 2x1 -u 1 -e 1 -n {str(n)} -v {str(verbose)}"
-
-        elif (moreaccurate == 1):
-            command = f"antsMotionCorr -d 3 -o [ants_mc_tmp/motcorr,ants_mc_tmp/motcorr.nii.gz,ants_mc_tmp/motcorr_avg.nii.gz] -m MI[ {fixed} , \
-                {moving} , 1 , {str(mibins)} , regular, 0.25 ] -t {txtype}[ 0.1 ] -i 100 -s 0 -f 1 -u 1 -e 1 -n {str(n)} -v {str(verbose)}"
-
-        elif (moreaccurate == 0):
-            command = f"antsMotionCorr -d 3 -o [ants_mc_tmp/motcorr,ants_mc_tmp/motcorr.nii.gz,ants_mc_tmp/motcorr_avg.nii.gz] -m MI[ {fixed} , \
-                {moving} , 1 , {str(mibins)} , regular, 0.02 ] -t {txtype}[ 0.1 ] -i 3 -s 0 -f 1 -u 1 -e 1 -n {str(n)} -v {str(verbose)}"
-        
-        elif (moreaccurate == "optim"):
-
-            # generate sensible smoothing coefficients based on image dimensions
-            low_dim = np.asarray(ref_img.GetSpacing()[:3]).min()
-            largest_dim = (np.array(ref_img.GetSize()[:3])*np.array(ref_img.GetSpacing()[:3])).max()
-
-            command=f'ants_generate_iterations.py --min {low_dim} --max {largest_dim}'
-            import subprocess
-            from nipype import logging
-            log = logging.getLogger('nipype.workflow')
-            log.debug('Running: '+command)
-            try:
-                process = subprocess.run(
-                    command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
-                    check=True,
-                    shell=True,
-                    )
-            except Exception as e:
-                log.warning(e.output.decode("utf-8"))
-                raise
-            out = process.stdout.decode("utf-8")
-            log.debug(out)
-
-            s = out.split('--smoothing-sigmas ')[-1].split('mm')[0].split('x')[-2:] # taking the last 2 smoothing sigmas
-            f = out.split('--shrink-factors ')[-1].split(' ')[0].split('x')[-2:] # taking shrink factors
-
-            if len(s)==1:
-                command = f"antsMotionCorr -d 3 -o [ants_mc_tmp/motcorr,ants_mc_tmp/motcorr.nii.gz,ants_mc_tmp/motcorr_avg.nii.gz] -m MI[ {fixed} , \
-                    {moving} , 1 , {str(mibins)} , Regular, 0.25, 1 ] -t {txtype}[ 0.1 ] -i 20 -s 0.0mm -f 1 -u 1 -e 1 -n {str(n)} -v {str(verbose)}"
-            elif len(s)>1:
-                command = f"antsMotionCorr -d 3 -o [ants_mc_tmp/motcorr,ants_mc_tmp/motcorr.nii.gz,ants_mc_tmp/motcorr_avg.nii.gz] -m MI[ {fixed} , \
-                    {moving} , 1 , {str(mibins)} , Regular, 0.25, 1 ] -t {txtype}[ 0.1 ] -i 50x20 -s {s[0]}x{s[1]}mm -f {f[0]}x{f[1]} -u 1 -e 1 -n {str(n)} -v {str(verbose)}"
-            else:
-                raise ValueError("No smoothing coefficient was found.")
-        else:
-            raise ValueError("Wrong moreaccurate provided.")
-        rc,c_out = run_command(command)
-
-        setattr(self, 'csv_params', os.path.abspath('ants_mc_tmp/motcorrMOCOparams.csv'))
-        setattr(self, 'mc_corrected_bold', os.path.abspath('ants_mc_tmp/motcorr.nii.gz'))
-        setattr(self, 'avg_image', os.path.abspath('ants_mc_tmp/motcorr_avg.nii.gz'))
+        transforms = framewise_register_pair(moving, ref_file, level=level)
+        csv_param_file = output_prefix + f"moco.csv"
+        write_transforms_to_csv(transforms, csv_param_file)
+        setattr(self, 'csv_params', csv_param_file)
 
         return runtime
 
@@ -226,124 +135,6 @@ class antsMotionCorr(BaseInterface):
                 'avg_image': getattr(self, 'avg_image')}
 
 
-def register_slice(fixed_image, moving_image):
-    # function for 2D registration
-    dimension = 2
-    initial_transform = sitk.Transform(dimension, sitk.sitkIdentity)
-
-    registration_method = sitk.ImageRegistrationMethod()
-
-    # Similarity metric settings.
-    registration_method.SetMetricAsMattesMutualInformation(
-        numberOfHistogramBins=20)
-    registration_method.SetMetricSamplingStrategy(registration_method.NONE)
-    # registration_method.SetMetricSamplingPercentage(0.01)
-
-    registration_method.SetInterpolator(sitk.sitkLinear)
-
-    # Optimizer settings.
-    registration_method.SetOptimizerAsGradientDescent(
-        learningRate=0.05, numberOfIterations=100, convergenceMinimumValue=1e-6, convergenceWindowSize=10)
-    # registration_method.SetOptimizerScalesFromPhysicalShift()
-
-    # Setup for the multi-resolution framework.
-    registration_method.SetShrinkFactorsPerLevel(shrinkFactors=[4, 2, 1])
-    registration_method.SetSmoothingSigmasPerLevel(smoothingSigmas=[2, 1, 0])
-    # registration_method.SmoothingSigmasAreSpecifiedInPhysicalUnitsOn()
-
-    # Don't optimize in-place, we would possibly like to run this cell multiple times.
-    registration_method.SetInitialTransform(initial_transform, inPlace=False)
-
-    final_transform = registration_method.Execute(sitk.Cast(fixed_image, sitk.sitkFloat32),
-                                                  sitk.Cast(moving_image, sitk.sitkFloat32))
-    return final_transform
-
-
-def slice_specific_registration(i, ref_file, timeseries_file):
-    from nipype import logging
-    log = logging.getLogger('nipype.workflow')
-
-    log.debug('Slice-specific correction on volume '+str(i+1))
-    ref_image = sitk.ReadImage(ref_file, sitk.sitkFloat32)
-    timeseries_image = sitk.ReadImage(timeseries_file, sitk.sitkFloat32)
-    volume_array = sitk.GetArrayFromImage(timeseries_image)[i, :, :, :]
-
-    for j in range(volume_array.shape[1]):
-        slice_array = volume_array[:, j, :]
-        if slice_array.sum()==0:
-            continue
-        moving_image = sitk.GetImageFromArray(slice_array)
-        fixed_image = ref_image[:, j, :]
-        moving_image.CopyInformation(fixed_image)
-
-        final_transform = register_slice(fixed_image, moving_image)
-        moving_resampled = sitk.Resample(moving_image, fixed_image, final_transform,
-                                         sitk.sitkLinear, 0.0, moving_image.GetPixelID())
-
-        resampled_slice = sitk.GetArrayFromImage(moving_resampled)
-        volume_array[:, j, :] = resampled_slice
-    return [i, volume_array]
-
-
-class SliceMotionCorrectionInputSpec(BaseInterfaceInputSpec):
-    in_file = File(exists=True, mandatory=True, desc='input BOLD time series')
-    ref_file = File(exists=True, mandatory=True,
-                    desc='ref file to realignment time series')
-    name_source = File(exists=True, mandatory=True,
-                       desc='Reference BOLD file for naming the output.')
-    n_procs = traits.Int(exists=True, mandatory=True,
-                         desc="Number of processors available to run in parallel.")
-
-
-class SliceMotionCorrectionOutputSpec(TraitedSpec):
-    mc_corrected_bold = File(exists=True, desc="motion corrected time series")
-
-
-class SliceMotionCorrection(BaseInterface):
-    """
-    This interface performs slice-specific motion realignment of coronal slices to correct for interslice
-    misalignment issues that arise from within-TR motion. It relies on 2D Rigid registration to the
-    reference 3D EPI volume provided.
-    """
-
-    input_spec = SliceMotionCorrectionInputSpec
-    output_spec = SliceMotionCorrectionOutputSpec
-
-    def _run_interface(self, runtime):
-
-        import multiprocessing as mp
-
-        timeseries_image = sitk.ReadImage(
-            self.inputs.in_file, sitk.sitkFloat32)
-
-        pool = mp.Pool(processes=self.inputs.n_procs)
-        results = [pool.apply_async(slice_specific_registration, args=(
-            i, self.inputs.ref_file, self.inputs.in_file)) for i in range(timeseries_image.GetSize()[3])]
-        results = [p.get() for p in results]
-        # enforce proper order of the slices
-        results.sort()
-        results = [r[1] for r in results]
-
-        timeseries_array = sitk.GetArrayFromImage(timeseries_image)
-        for i in range(timeseries_image.GetSize()[3]):
-            timeseries_array[i, :, :, :] = results[i]
-
-        # clip potential negative values
-        timeseries_array[(timeseries_array < 0).astype(bool)] = 0
-        resampled_timeseries = sitk.GetImageFromArray(
-            timeseries_array, isVector=False)
-        resampled_timeseries.CopyInformation(timeseries_image)
-
-        split = pathlib.Path(self.inputs.name_source).name.rsplit(".nii")
-        out_name = os.path.abspath(split[0]+'_slice_mc.nii.gz')
-        sitk.WriteImage(resampled_timeseries, out_name)
-
-        setattr(self, 'mc_corrected_bold', out_name)
-
-        return runtime
-
-    def _list_outputs(self):
-        return {'mc_corrected_bold': getattr(self, 'mc_corrected_bold')}
 
 
 class EstimateMotionParamsInputSpec(BaseInterfaceInputSpec):
