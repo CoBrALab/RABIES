625428ed658905d5c3f69eb628a808914b8c09e1
Gab-D-G <gabriel.desrosiers-gregoire@mail.mcgill.ca>
2026-01-16 14:03:49 +0100

    Implemented a preliminary class for generating a HMC QC report.

--- rabies/preprocess_pkg/hmc.py
+++ rabies/preprocess_pkg/hmc.py
@@ -219,3 +219,192 @@ def motion_24_params(movpar_csv):
                     'mov1^2', 'mov2^2', 'mov3^2', 'rot1^2', 'rot2^2', 'rot3^2', 'mov1_der^2', 'mov2_der^2', 'mov3_der^2', 'rot1_der^2', 'rot2_der^2', 'rot3_der^2']
 
     return movpar,motion_24_header
+
+
+class HMC_QCInputSpec(BaseInterfaceInputSpec):
+    in_file = File(exists=True, mandatory=True, desc='input BOLD time series')
+    ref_file = File(exists=True, mandatory=True,
+                    desc='ref file to realignment time series')
+    out_dir = traits.Str(mandatory=True, desc="Directory for QC outputs.")
+    csv_params = File(
+        exists=True, desc="csv files with the 6-parameters rigid body transformations")
+    figure_format = traits.Str(default='png',
+        mandatory=True, exists=True, desc="png or svg")
+
+
+class HMC_QCOutputSpec(TraitedSpec):
+    out_figure = File(exists=True, desc="Output figure.")
+    video_file = File(exists=True, desc="Output video.")
+
+
+class HMC_QC(BaseInterface):
+    """
+
+    """
+
+    input_spec = HMC_QCInputSpec
+    output_spec = HMC_QCOutputSpec
+
+    def _run_interface(self, runtime):
+
+        filename = pathlib.Path(self.inputs.in_file).name.rsplit(".nii")[0]
+        out_dir = self.inputs.out_dir
+        os.makedirs(out_dir, exist_ok=True)
+        figure_path = f'{out_dir}/{filename}_HMC_QC.{self.inputs.figure_format}'
+
+        derivatives_dict = HMC_derivatives(self.inputs.in_file, self.inputs.ref_file, self.inputs.csv_params)
+        fig = plot_motion_QC(derivatives_dict)
+        fig.savefig(figure_path, bbox_inches='tight')
+
+        # write .webp file
+        video_file = f'{out_dir}/{filename}_HMC.webp'
+        from rabies.simpleitk_timeseries_motion_correction.create_animation import main
+        main(input_img=derivatives_dict['img_preHMC'], output_file=video_file, second_input_img=derivatives_dict['img_postHMC'], scale=2.0, fps=10)
+
+        setattr(self, 'out_figure', figure_path)
+        setattr(self, 'video_file', video_file)
+
+        return runtime
+
+    def _list_outputs(self):
+        return {'out_figure': getattr(self, 'out_figure'),
+                'video_file': getattr(self, 'video_file'),
+                }
+
+
+#############FUNCTIONS FOR HMC QC
+import numpy as np
+import SimpleITK as sitk
+from rabies.utils import copyInfo_3DImage
+
+def cosine_similarity(X,Y): 
+    X_ = X.copy()
+    X_ /= np.sqrt((X_ ** 2).sum(axis=0)) # first normalize from l2-norm
+    Y_ = Y.copy()
+    Y_ /= np.sqrt((Y_ ** 2).sum(axis=0)) # first normalize from l2-norm
+    
+    Sc = (X_.T.dot(Y_)) # then derive vector product across columns
+    return Sc
+
+
+def get_SD(img):
+    array = sitk.GetArrayFromImage(img)
+    std = array.std(axis=0)
+    std_image = copyInfo_3DImage(
+        sitk.GetImageFromArray(std, isVector=False), img)
+    return std_image
+
+
+def get_motion_R2(timeseries_img, translations,rotations):
+    from rabies.analysis_pkg.analysis_math import closed_form
+    timeseries_4d = sitk.GetArrayFromImage(timeseries_img)
+    timeseries = timeseries_4d.reshape(timeseries_4d.shape[0],-1).astype(float)
+    confounds_array = np.concatenate((translations,rotations), axis=1)
+    
+    X=confounds_array-confounds_array.mean(axis=0)
+    Y=timeseries-timeseries.mean(axis=0)
+    res = Y-X.dot(closed_form(X,Y))
+    R2_spatial = 1-(res.var(axis=0)/Y.var(axis=0))
+
+    R2_img = sitk.GetImageFromArray(R2_spatial.reshape(timeseries_4d.shape[1:]))
+    R2_img = copyInfo_3DImage(R2_img, timeseries_img)
+    return R2_img
+
+
+def HMC_derivatives(in_file, ref_file, motcorr_params_file):
+    import pandas as pd
+    from rabies.simpleitk_timeseries_motion_correction.apply_transforms import read_transforms_from_csv, framewise_resample_volume
+    img_preHMC = sitk.ReadImage(in_file)
+    ref_img = sitk.ReadImage(ref_file)
+    
+    # prepare timeseries post-correction
+    transforms = read_transforms_from_csv(motcorr_params_file)
+    img_postHMC = framewise_resample_volume(img_preHMC, ref_img, transforms, interpolation=sitk.sitkBSpline5, clip_negative=False, extrapolator=False)
+    
+    df = pd.read_csv(motcorr_params_file)
+    translations = np.array([df[par] for par in ['TransX', 'TransY', 'TransZ']]).T
+    rotations = np.array([df[par] for par in ['EulerX', 'EulerY', 'EulerZ']]).T
+
+    num_volumes = img_preHMC.GetSize()[3]
+    ref_img_array = sitk.GetArrayFromImage(ref_img).reshape(-1,1).astype(float)
+    timeseries_preHMC = sitk.GetArrayFromImage(img_preHMC).reshape(num_volumes,-1).astype(float)
+    timeseries_postHMC = sitk.GetArrayFromImage(img_postHMC).reshape(num_volumes,-1).astype(float)
+    
+    D_Sc_preHMC = 1-cosine_similarity(timeseries_preHMC.T,ref_img_array)
+    D_Sc_postHMC = 1-cosine_similarity(timeseries_postHMC.T,ref_img_array)
+
+    img_preHMC_SD = get_SD(img_preHMC)
+    img_postHMC_SD = get_SD(img_postHMC)
+
+    img_preHMC_R2 = get_motion_R2(img_preHMC, translations,rotations)
+    img_postHMC_R2 = get_motion_R2(img_postHMC, translations,rotations)
+
+    return {'img_preHMC':img_preHMC, 'img_postHMC':img_postHMC, 'translations':translations, 'rotations':rotations, 'D_Sc_preHMC':D_Sc_preHMC, 'D_Sc_postHMC':D_Sc_postHMC, 
+            'img_preHMC_SD':img_preHMC_SD, 'img_postHMC_SD':img_postHMC_SD, 'img_preHMC_R2':img_preHMC_R2, 'img_postHMC_R2':img_postHMC_R2}
+
+
+def plot_motion_QC(derivatives_dict):
+    import matplotlib.pyplot as plt
+    from rabies.visualization import plot_3d
+    fig,axes = plt.subplots(nrows=9, ncols=2, figsize=(8*2,2*9), constrained_layout=True)
+    
+    ax_fused_l = []
+    for i in range(3):
+        gs = axes[i, 0].get_gridspec()
+        # remove axes you want to fuse
+        axes[i, 0].remove()
+        axes[i, 1].remove()
+        # add fused axis
+        ax_fused = fig.add_subplot(gs[i, 0:2])
+        ax_fused_l.append(ax_fused)
+    
+    ax=ax_fused_l[0]
+    ax.plot(derivatives_dict['translations'])
+    ax.set_ylabel('mm', fontsize=15, color='white')
+    ax.set_title('Translations', fontsize=20, color='white')
+    ax.tick_params(labelsize=15)
+    
+    ax=ax_fused_l[1]
+    ax.plot(derivatives_dict['rotations'])
+    ax.set_ylabel('Euler angle (radians)', fontsize=15, color='white')
+    ax.set_title('Rotations', fontsize=20, color='white')
+    ax.tick_params(labelsize=15)
+    
+    ax=ax_fused_l[2]
+    ax.plot(derivatives_dict['D_Sc_preHMC'])
+    ax.plot(derivatives_dict['D_Sc_postHMC'])
+    ax.set_ylabel('Cosine distance', fontsize=15, color='white')
+    ax.set_title('Difference relative to reference volume', fontsize=20, color='white')
+    ax.legend(['Before correction','After correction'])
+    ax.tick_params(labelsize=15)
+    
+    #### Plot brain images    
+    SD_array = sitk.GetArrayFromImage(derivatives_dict['img_preHMC_SD']).flatten()
+    SD_array.sort()
+    std_vmax=SD_array[int(len(SD_array)*0.99)]
+    
+    ax_3d=axes[3:6,0]
+    ax_3d[0].set_title('Temporal standard deviation\n(before correction)', fontsize=20, color='white')
+    cbar_list = plot_3d(ax_3d,derivatives_dict['img_preHMC_SD'],fig=fig,vmin=0,vmax=std_vmax,cmap='inferno', cbar=True)
+    for cbar in cbar_list:
+        cbar.ax.tick_params(labelsize=15)
+    
+    ax_3d=axes[6:,0]
+    ax_3d[0].set_title('Temporal standard deviation\n(after correction)', fontsize=20, color='white')
+    cbar_list = plot_3d(ax_3d,derivatives_dict['img_postHMC_SD'],fig=fig,vmin=0,vmax=std_vmax,cmap='inferno', cbar=True)
+    for cbar in cbar_list:
+        cbar.ax.tick_params(labelsize=15)
+    
+    ax_3d=axes[3:6,1]
+    ax_3d[0].set_title('R2 from 6 motion parameters\n(before correction)', fontsize=20, color='white')
+    cbar_list = plot_3d(ax_3d,derivatives_dict['img_preHMC_R2'],fig=fig,vmin=0,vmax=1,cmap='inferno', cbar=True)
+    for cbar in cbar_list:
+        cbar.ax.tick_params(labelsize=15)
+    
+    ax_3d=axes[6:,1]
+    ax_3d[0].set_title('R2 from 6 motion parameters\n(after correction)', fontsize=20, color='white')
+    cbar_list = plot_3d(ax_3d,derivatives_dict['img_postHMC_R2'],fig=fig,vmin=0,vmax=1,cmap='inferno', cbar=True)
+    for cbar in cbar_list:
+        cbar.ax.tick_params(labelsize=15)
+        
+    return fig
